3db96f9b6337939f4cc57c2312f56d87d45b4c7a
Visesh Rajendraprasad <viseshrprasad@gmail.com>
2026-01-23 22:11:48 -0500

    chore: branding and icons

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,9 +2,27 @@ import { defineConfig } from 'wxt';
 
 export default defineConfig({
   manifest: {
+    name: 'nufftabs',
+    description: 'Enough tabs. Condense and restore.',
     permissions: ['tabs', 'storage'],
     action: {
       default_title: 'Condense tabs',
+      default_icon: {
+        16: 'icon/16.png',
+        19: 'icon/19.png',
+        32: 'icon/32.png',
+        38: 'icon/38.png',
+        48: 'icon/48.png',
+        128: 'icon/128.png',
+      },
+    },
+    icons: {
+      16: 'icon/16.png',
+      19: 'icon/19.png',
+      32: 'icon/32.png',
+      38: 'icon/38.png',
+      48: 'icon/48.png',
+      128: 'icon/128.png',
     },
     options_ui: {
       page: 'options.html',
