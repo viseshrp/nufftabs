afee39f71ab86cc4f33d4ce1c12f49ec41b83272
viseshrp <viseshrprasad@gmail.com>
2026-02-16 16:38:25 -0500

    handle background click condense rejections

--- entrypoints/background/index.ts
+++ entrypoints/background/index.ts
@@ -4,7 +4,9 @@ export { condenseCurrentWindow };
 
 export function registerActionClickHandler(): void {
   chrome.action.onClicked.addListener((tab) => {
-    void condenseCurrentWindow(tab?.windowId);
+    void condenseCurrentWindow(tab?.windowId).catch((error: unknown) => {
+      console.error('Failed to condense current window:', error);
+    });
   });
 }
 
